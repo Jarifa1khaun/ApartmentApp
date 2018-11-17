843f6f353f07b7bcce44063dc60147464f0887eb
Jarifa Khatun <jarifamustafa.aapshari@gmail.com>
2018-11-17 17:37:25 +0600

    setup for dockerless development env and update for postman

---
+++ NodeBackEnd/code/withoutDocker.bat
@@ -0,0 +1,8 @@
+set NODE_ENV=development
+set CONTAINER_PORT=3000
+set JWT_PRIVATE_KEY=Y9zRh0yx2M3PLxsWAnmd
+set API_KEY=AIzaSyDWNE_jdj3NJj4IUWwz4kCRUg3k0hjVzWc
+set DB_URI=mongodb://dev_jarifa:devJarifa123@ds159651.mlab.com:59651/khoj_the_search
+set SESSION_DURATION=1440
+npm install
+npm start
