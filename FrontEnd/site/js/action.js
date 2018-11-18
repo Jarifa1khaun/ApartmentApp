51c8d12e457619e2eec72f38eb17118052c3f0af
Jarifa Khatun <jarifamustafa.aapshari@gmail.com>
2018-11-18 22:58:41 +0600

    test code updated

--- FrontEnd/site/js/action.js
+++ FrontEnd/site/js/action.js
@@ -1,4 +1,4 @@
-const address = "http://localhost:3000/api/users/";
+const address = "http://localhost:3000/api/advertisement/?pageNumber=1&pageSize=5";
 // const address = "https://jsonplaceholder.typicode.com/posts/2";
 
 $('#clickMe').click(getData);
@@ -9,6 +9,7 @@ function getData() {
 
 function output(data) {
     console.log(data);
-    $('#output').html('<h2>' + data.id + ': ' + data.name + '</h2><p>');
+    const item = data[0];
+    $('#output').html('<h2>' + 'House name: ' + item.name + ', contact number: ' + item.contact_number + '</h2><p>');
     // $('#output').html('<h2>' + data.title + ': ' + data.body + '</h2><p>');
 }
