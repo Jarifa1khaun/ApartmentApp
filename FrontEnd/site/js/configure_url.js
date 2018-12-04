598a5d8a7401f9d0dcf6b7ab44e7fd80c3920cb4
Jarifa Khatun <jarifamustafa.aapshari@gmail.com>
2018-12-05 02:27:34 +0600

    url configure modified

--- FrontEnd/site/js/configure_url.js
+++ FrontEnd/site/js/configure_url.js
@@ -1,8 +1,10 @@
 var server_url = "SERVER_IP";
 
+var letters = /^[.0-9]+$/;
+
 function getBackEndIPAddress() {
 
-    if (server_url === 'SERVER_IP') {
+    if (server_url.match(letters) === null) {
         return 'localhost';
     } else {
         return server_url;
