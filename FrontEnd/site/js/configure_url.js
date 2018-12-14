4997c0faa5abf2aa597ef501e3e8de7381fc8951
Jarifa Khatun <jarifamustafa.aapshari@gmail.com>
2018-12-14 16:19:33 +0600

    reduced 3

--- FrontEnd/site/js/configure_url.js
+++ FrontEnd/site/js/configure_url.js
@@ -1,12 +1,4 @@
-var server_url = "SERVER_IP";
-
-var letters = /^[_a-zA-Z]+$/;
-
 function getBackEndIPAddress() {
 
-    if (server_url.match(letters) === null) {
-        return server_url;
-    } else {
-        return 'localhost';
-    }
+    return 'localhost';
 }
